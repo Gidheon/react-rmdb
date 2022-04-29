d226b887566664512e4b86389216cc36914bc125
Eduardo Ubilla <eubillac@emeal.nttdata.com>
2022-04-29 15:08:50 -0400

    rmdb4

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React, { Fragment } from "react";
-//Components
-import Header from "./components/Header";
-import Home from "./components/Home";
+import React from 'react';
+// Routing
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// Components
+import Header from './components/Header';
+import Home from './components/Home';
+import Movie from './components/Movie';
+import NotFound from './components/NotFound';
 
-//Styles
-import { GlobalStyle } from "./GlobalStyles";
+// Styles
+import { GlobalStyle } from './GlobalStyles';
 
-function App() {
-  return (
-    <Fragment>
-      <Header />
-      <Home />
-      <GlobalStyle />
-    </Fragment>
-  );
-}
+const App = () => (
+  <Router>
+    <Header />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/:movieId' element={<Movie />} />
+      <Route path='/*' element={<NotFound />} />
+    </Routes>
+    <GlobalStyle />
+  </Router>
+);
 
 export default App;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-import RMDLogo from '../../images/react-movie-logo.svg';
+import RMDLogo from "../../images/react-movie-logo.svg";
 import TMDBLogo from "../../images/tmdb_logo.svg";
 
 import { Wrapper, Content, LogoImg, TMDBLogoImg } from "./Header.styles";
 
 const Header = () => (
-
-    <Wrapper>
-        <Content>
-            <LogoImg src={RMDLogo} alt='rmdb-logo' />
-            <TMDBLogoImg src={TMDBLogo} alt='tmdb-logo' />
-        </Content>
-    </Wrapper>
-)
+  <Wrapper>
+    <Content>
+      <Link to="/">
+        <LogoImg src={RMDLogo} alt="rmdb-logo" />
+      </Link>
+      <TMDBLogoImg src={TMDBLogo} alt="tmdb-logo" />
+    </Content>
+  </Wrapper>
+);
 
 export default Header;
